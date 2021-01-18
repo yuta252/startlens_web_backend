f769b0016d466e29f867236f4c165d031685f1f4
Yuta Nakano <nakano.yuta252@gmail.com>
2021-01-19 02:33:30 +0900

    [add]create API endpoint that outputs user statistic data for dashboard #55

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       resources :multi_profiles, only: [:index, :create, :update, :destroy]
       resources :exhibits, only: [:index, :create, :update, :destroy]
       resources :multi_exhibits, only: [:create, :update, :destroy]
+      resources :dashboards, onley: [:index]
 
       namespace :tourist do
         post '/token', to: 'tokens#create', as: :tokens
