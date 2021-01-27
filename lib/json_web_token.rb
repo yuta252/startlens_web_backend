39c213b2653e8989d8978f9eb8cb43bb54182457
Yuta Nakano <nakano.yuta252@gmail.com>
2021-01-27 14:25:21 +0900

    [fix]update image uploader to escape from other parameter validation

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,8 +1,7 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
-  def self.encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp.to_i
+  def self.encode(payload)
     JWT.encode(payload, SECRET_KEY)
   end
 
