9a251585f9da07244e1b04fadaddad83ed6780cb
Yuta Nakano <nakano.yuta252@gmail.com>
2021-02-06 22:20:06 +0900

    [update]change exhibit image file path for learning

--- app/uploaders/exhibit_image_uploader.rb
+++ app/uploaders/exhibit_image_uploader.rb
@@ -21,7 +21,7 @@ class ExhibitImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{model.exhibit.user.id}/#{model.exhibit.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
