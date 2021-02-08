acd718b0e2ecc4a595ddc68ce7d88e916bd30dd7
Yuta Nakano <nakano.yuta252@gmail.com>
2021-02-08 14:22:49 +0900

    [add]create batch job for registering user logs #66

---
+++ lib/batch/log_create.rb
@@ -0,0 +1,10 @@
+class Batch::LogCreate
+  def self.log_create
+    user = UserStatistic.new(
+      user_id: 1,
+      tourist_id: rand(1..100).to_i
+    )
+    user.save(validate: false)
+    p "create new log"
+  end
+end
