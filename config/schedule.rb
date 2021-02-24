fa29903777f2641c93a3f42dd50a5263aaacc08f
Yuta Nakano <nakano.yuta252@gmail.com>
2021-02-24 13:22:49 +0900

    [update]change cors settings and batch operation

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,7 +12,7 @@ every 25.minutes do
   end
 end
 
-every 34.minutes do
+every 1.days, at: '11:00' do
   begin
     runner "Batch::LogCreate.log_create"
   rescue => e
@@ -21,7 +21,7 @@ every 34.minutes do
   end
 end
 
-every 1.days, at: '13:00 am' do
+every 1.days, at: '13:00' do
   begin
     runner "Batch::LogCreate.log_create"
   rescue => e
@@ -30,7 +30,43 @@ every 1.days, at: '13:00 am' do
   end
 end
 
-every 3.days do
+every 1.days, at: '15:00' do
+  begin
+    runner "Batch::LogCreate.log_create"
+  rescue => e
+    Rails.logger.error("aborted rails runner")
+    raise e
+  end
+end
+
+every 3.days, at: '14:20' do
+  begin
+    runner "Batch::LogCreate.log_create"
+  rescue => e
+    Rails.logger.error("aborted rails runner")
+    raise e
+  end
+end
+
+every 4.days, at: '17:20' do
+  begin
+    runner "Batch::LogCreate.log_create"
+  rescue => e
+    Rails.logger.error("aborted rails runner")
+    raise e
+  end
+end
+
+every :saturday, at: '11:30' do
+  begin
+    runner "Batch::LogCreate.log_create"
+  rescue => e
+    Rails.logger.error("aborted rails runner")
+    raise e
+  end
+end
+
+every :sunday, at: '12:30' do
   begin
     runner "Batch::LogCreate.log_create"
   rescue => e
