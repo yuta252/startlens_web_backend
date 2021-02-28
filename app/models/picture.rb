3d2b7d598b54b1e33cdee69e0115c8b4644e0401
Yuta Nakano <nakano.yuta252@gmail.com>
2021-02-28 22:30:46 +0900

    [delete]Delete picture model log

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -16,12 +16,9 @@ class Picture < ApplicationRecord
         picture_params[:image_file].each do |picture|
           image_data = self.split_base64(picture.to_s)
           image_data_string = image_data[:data]
-          logger.debug(image_data_string)
           io = CarrierStringIO.new(Base64.decode64(image_data_string))
           new_picture = Picture.new(exhibit_id: exhibit_id, image: io)
-          logger.debug("before picture save")
           new_picture.save!
-          logger.debug("success picture save")
         end
       end
     rescue => exception
